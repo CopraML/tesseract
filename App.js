97a06758b9762148157d2e3992994d820379cb1b
Varghese Thomas <varghesethomase@gmail.com>
2017-11-11 20:31:39 +0530

    Basic app with ability to take a picture developed

--- App.js
+++ App.js
@@ -1,57 +1,100 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * ./index.js
+ * @author  Jonathan Palma <tanpalma04@gmail.com>
  */
-
+'use strict';
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Image, Platform, PixelRatio, StyleSheet, Text, TouchableNativeFeedback, TouchableOpacity, View } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import RNTesseractOcr from 'react-native-tesseract-ocr';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+var Button = (Platform.OS === 'android') ? TouchableNativeFeedback : TouchableOpacity;
 
-export default class App extends Component<{}> {
+export default class App extends React.Component {
+  constructor(props, context){
+    super(props, context);
+    this.state = { imgSource: null, ocrResult: null };
+  }
+
+  selectPhoto(){
+    const options = {
+      quality: 1.0,
+      maxWidth: 500,
+      maxHeight: 500,
+      storageOptions: {
+        skipBackup: true
+      }
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        var source;
+
+        if (Platform.OS === 'android') {
+          source = {uri: response.uri, isStatic: true};
+        } else {
+          source = {uri: response.uri.replace('file://', ''), isStatic: true};
+        }
+
+        this.setState({ imgSource: source });
+
+        RNTesseractOcr.startOcr(response.path, "LANG_ENGLISH")
+          .then((result) => {
+            this.setState({ ocrResult: result });
+            console.log("OCR Result: ", result);
+          })
+          .catch((err) => {
+            console.log("OCR Error: ", err);
+          })
+          .done();
+      }
+    });
+  }
+
   render() {
-    return (
+    return(
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! lorem ipsum adfasd
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button onPress={this.selectPhoto.bind(this)} >
+          <View style={[styles.img, styles.imgContainer, {marginBottom: 20}]}>
+          { this.state.imgSource === null ? <Text>Select a Photo</Text> :
+            <Image style={styles.img} source={this.state.imgSource} />
+          }
+          </View>
+        </Button>
+
+        <Text>OCR Result: {this.state.ocrResult}</Text>
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  imgContainer: {
+    borderColor: '#9B9B9B',
+    borderWidth: 1 / PixelRatio.get(),
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  img: {
+    borderRadius: 75,
+    width: 150,
+    height: 150
+  }
 });
